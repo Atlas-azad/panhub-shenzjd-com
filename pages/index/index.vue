15d9e7d64fcdb40cdf3718819d9d2b9b4ba5c951
Atlas-azad <132842845+Atlas-azad@users.noreply.github.com>
2026-09-13 15:14:58 +0800

    Update index.vue

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -89,7 +89,7 @@
                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/></svg>
                 <span>免责声明</span>
               </div>
-              <p class="tips-disclaimer-line">本站仅提供网盘资源搜索服务，不存储任何文件内容</p>
+              <p class="tips-disclaimer-line">本站仅提供资源搜索服务，不存储任何文件内容</p>
               <p class="tips-disclaimer-line">所有资源均来自互联网公开分享，版权归原作者所有</p>
               <p class="tips-disclaimer-line">用户下载使用资源时请遵守相关法律法规</p>
               <p class="tips-disclaimer-contact">
